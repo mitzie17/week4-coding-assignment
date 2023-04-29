ee91d9e83755faf98ca14dd0477d7ec405d5f087
Mitzi17 <m_estrada17@live.com>
2023-04-28 17:43:21 -0700

    solved questions 1 and 2

--- index.js
+++ index.js
@@ -6,13 +6,48 @@ let ages = [3, 9, 23, 64, 2, 8, 28, 93]
 // 1a. Programmatically subtract the value of the first element in the array from the value in the last element of the array 
 
 let subtraction = ages[ages.length - 1] - ages[0]
-console.log(subtraction)
+console.log('1a. ' + subtraction)
 
 // 1b. Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
 
-let newAges = ages.push(50)
-console.lo
-let newSubtraction = newAges[newAges.length - 1] - newAges[0]
-console.log(newSubtraction)
+ages.push(50)
+let subtraction2 = ages[ages.length - 1] - ages[0]
+console.log('1b. ' + subtraction2)
 
-//Use a loop to iterate through the array and calculate the average age
+
+// 1c.Use a loop to iterate through the array and calculate the average age
+let sum = 0;
+for (i=0; i < ages.length; i++) {
+    sum += ages[i];
+}
+let avg = sum / ages.length
+console.log('1c. Average age is ' + avg)
+
+// 2. Create an array called names that contains the following values: ‘Sam’, ‘Tommy’, ‘Tim’, ‘Sally’, ‘Buck’, ‘Bob’.
+
+let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']
+
+// 2a. Use a loop to iterate through the array and calculate the average number of letters per name.
+let sumLetters = 0
+for (name of names) {
+    sumLetters += name.length;
+}
+
+let average = sumLetters / names.length;
+console.log('2a. Average number of letters per name is ' + average)
+
+
+// 2b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
+let allNames = ''
+for (name of names) {
+    allNames += name + ' '
+}
+console.log('2b. ' + allNames)
+
+// 3. How do you access the last element of any array?
+
+//  4. How do you access the first element of any array?
+
+//
+
+
