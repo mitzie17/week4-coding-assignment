1af5151cf1bce4fb8f17f0cedf521f73810ec5f1
Mitzi17 <m_estrada17@live.com>
2023-04-28 21:27:51 -0700

    solved questions 6, 7, and 8

--- index.js
+++ index.js
@@ -48,18 +48,54 @@ console.log('2b. ' + allNames)
 // To access the last element of an array, you can call the lenght property on the array and subtract one.
 // The length property will return the number of items in the array but because arrays are zero-indexed, that is the first index in the array is zero,
 // we would need to subtract one to get the last item/element.
- console.log('To access the last element of ages array: ages[ages.length - 1]')
+ console.log('3. To access the last element of ages array: ages[ages.length - 1]')
 
 //  4. How do you access the first element of any array?
 // To access the first element of an array we need to type the first index number of the array inside the brackets, which is always zero.
-console.log('To access the first element of the ages array: ages[0]')
+console.log('4. To access the first element of the ages array: ages[0]')
 
 // 5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
 let nameLengths = []
 for (name of names) {
     nameLengths.push(name.length)
 }
-console.log(`New array with the length of each name: ${nameLengths}`)
+console.log(`5. The nameLengths array with the length of each name: ${nameLengths}`)
 
+// 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.
+let sumTotal = 0;
+for (i = 0; i < nameLengths.length; i++) {
+    sumTotal += nameLengths[i]
+}
+console.log('6. The total sum of the nameLengths array is ' + sumTotal)
 
+// 7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of tiems.
+// (i.e. if I pass in ‘Hello’ and 3, I would expect the function to return ‘HelloHelloHello’)
 
+function repeatWord (word, n) {
+    let repeatedWord = ''
+    for (i = 0; i < n; i++) {
+        repeatedWord += word
+    }
+    return repeatedWord;
+}
+console.log('7. ' + repeatWord('Hello', 3))
+
+// 8. Write a function that takes two parameters, firstName and lastName, and returns a full name.
+// The full name should be the first and the last name separated by a space.
+
+function fullName (firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
+console.log('8. fullName function returns ' + fullName('Mitzi', 'Estrada'))
+
+// 9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
+
+// 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
+
+// 11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
+
+// 12. Write a function call willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if is hot outside
+// and if moneyInPocket is greater than 10.50.
+
+// 13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
